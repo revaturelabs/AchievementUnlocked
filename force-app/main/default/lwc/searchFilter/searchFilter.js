0d1e07bf5f96f779d88d2efafaeb07e8a1ddf42f
Blake Prescott <blakewillprescott@gmail.com>
2021-08-31 15:49:21 -0500

    populate filter list with status field

--- force-app/main/default/lwc/searchFilter/searchFilter.js
+++ force-app/main/default/lwc/searchFilter/searchFilter.js
@@ -15,16 +15,14 @@ const DELAY = 350;
 
 export default class SearchFilter extends LightningElement {
     searchKey = '';
-    
 
     filters = {
-        searchKey: '',
-       
+        searchKey: ''     
     };
 
 
-   // @wire(MessageContext)
-   // messageContext;
+    @wire(MessageContext)
+    messageContext;
 
 
     @wire(getPicklistValues, {
@@ -33,12 +31,12 @@ export default class SearchFilter extends LightningElement {
     })
     statuses;
 
-
     
     handleSearchKeyChange(event) {
         this.filters.searchKey = event.target.value;
         this.delayedFireFilterChangeEvent();
     }
+    
 
     handleCheckboxChange(event) {
         if (!this.filters.statuses) {
@@ -46,8 +44,8 @@ export default class SearchFilter extends LightningElement {
             this.filters.statuses = this.statuses.data.values.map(
                 (item) => item.value
             );
-           
         }
+
         const value = event.target.dataset.value;
         const filterArray = this.filters[event.target.dataset.filter];
         if (event.target.checked) {
@@ -66,8 +64,6 @@ export default class SearchFilter extends LightningElement {
     }
 
 
-
-
     delayedFireFilterChangeEvent() {
         // Debouncing this method: Do not actually fire the event as long as this function is
         // being called within a delay of DELAY. This is to avoid a very large number of Apex
@@ -81,9 +77,4 @@ export default class SearchFilter extends LightningElement {
             });
         }, DELAY);
     }
-
-
-
-
-
 }
