d10bd3a44dfcee09af010c4c1a407234adcd6d69
Dominic Romanello <55882645+dominicromanello@users.noreply.github.com>
2021-09-01 14:26:03 -0400

    Delete force-app/main/default directory

--- force-app/main/default/lwc/stagingView/stagingView.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, wire, api } from 'lwc';
-
-export default class StagingView extends LightningElement {
-
-    constructor() {
-        super();
-        this.status = 'Staging'
-    }
-}
