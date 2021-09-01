64783f3a94db2793965a3eb21857c7e30f3d5515
ZackFra <zack11frazier22@gmail.com>
2021-09-01 17:48:40 -0400

    fixed weird quotes added to it

--- D3/lwc/pieChart/__tests__/pieChart.test.js
+++ D3/lwc/pieChart/__tests__/pieChart.test.js
@@ -1,6 +1,13 @@
 import { createElement } from "lwc";
 import PieChart from "c/pieChart";
 
+function mapDOM(root) {
+  for (const child of root.children) {
+    mapDOM(child);
+  }
+  console.log(root.tagName);
+}
+
 describe("c-pie-chart", () => {
   afterEach(() => {
     // The jsdom instance is shared across test cases in a single file so reset the DOM
@@ -30,6 +37,7 @@ describe("c-pie-chart", () => {
       const svg = element.shadowRoot.querySelector("svg");
       const legend = element.shadowRoot.querySelector("#legend");
       expect(svg).not.toBeNull();
+      expect(legend).not.toBeNull();
     });
   });
 });
