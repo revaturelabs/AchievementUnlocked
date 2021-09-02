eb913c9e3450a7e95ad784a2cc68e680a9b3fbc8
Gregory Mannerberg <gmannerberg@gmail.com>
2021-09-02 11:31:55 -0400

    cohort view version B

--- force-app/main/default/lwc/cohortView/cohortView.js
+++ force-app/main/default/lwc/cohortView/cohortView.js
@@ -7,4 +7,19 @@ export default class CohortView extends LightningElement {
     displayCohort(event) {
         this.cohortId = event.detail;
     }
+
+    handleToggleSection(event) {
+        const openSections = event.detail.openSections;
+        if (openSections == 'cohorts') {
+            this.cohortId = null;
+        }
+    }
+
+    renderedCallback() {
+        if (this.cohortId != null) {
+            const accordion = this.template.querySelector('.accordion');
+            accordion.activeSectionName = 'associates';
+        }
+    }
+
 }
