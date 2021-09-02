c1b4abc5ff7bdf1d094613895a6b6315f1d719b3
Blake Prescott <blakewillprescott@gmail.com>
2021-09-02 15:45:32 -0500

    Created Cert list, editted viewController and LWCs

--- force-app/main/default/lwc/voucherList/voucherList.js
+++ force-app/main/default/lwc/voucherList/voucherList.js
@@ -2,8 +2,9 @@ import { LightningElement, api, wire } from 'lwc';
 import getVouchers from '@salesforce/apex/viewController.getVouchers';
 
 const columns = [
-    { label: 'Name', fieldName: 'Name' },
+    { label: 'Voucher', fieldName: 'Name' },
     { label: 'Certification', fieldName: 'Certification_Type__c'},
+    { label: 'Voucher Type', fieldName: 'Voucher_Type__c' },
     { label: 'Status', fieldName: 'Status__c' },
 ];
 
