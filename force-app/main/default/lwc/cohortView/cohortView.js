6392fb227266a41615494fee19bb1290f2461966
gregory-mannerberg <86627319+gregory-mannerberg@users.noreply.github.com>
2021-09-06 01:06:40 -0400

    Update cohortView.js

--- force-app/main/default/lwc/cohortView/cohortView.js
+++ force-app/main/default/lwc/cohortView/cohortView.js
@@ -1,13 +1,13 @@
 import {LightningElement, wire} from 'lwc';
-import {MessageContext, publish} from 'lightning/messageService';
-import COHORT_SELECTED_CHANNEL from '@salesforce/messageChannel/CohortSelected__c';
+//import {MessageContext, publish} from 'lightning/messageService';
+//import COHORT_SELECTED_CHANNEL from '@salesforce/messageChannel/CohortSelected__c';
 
 export default class CohortView extends LightningElement {
 
     filter = 'active';
     cohortId = null;
-    @wire(MessageContext)
-    messageContext;
+    //@wire(MessageContext)
+    //messageContext;
     
     handleChange(event) {
         this.filter = event.detail.value;
@@ -23,20 +23,20 @@ export default class CohortView extends LightningElement {
 
     displayCohort(event) {
         this.cohortId = event.detail;
-        const payload = {
+        /*const payload = {
             cohortId: this.cohortId
         };
-        publish(this.messageContext, COHORT_SELECTED_CHANNEL, payload);
+        publish(this.messageContext, COHORT_SELECTED_CHANNEL, payload);*/
     }
 
     handleToggleSection(event) {
         const openSections = event.detail.openSections;
         if (openSections == 'cohorts') {
             this.cohortId = null;
-            const payload = {
+            /*const payload = {
                 cohortId: this.cohortId
             };
-            publish(this.messageContext, COHORT_SELECTED_CHANNEL, payload);
+            publish(this.messageContext, COHORT_SELECTED_CHANNEL, payload);*/
         }
     }
 
