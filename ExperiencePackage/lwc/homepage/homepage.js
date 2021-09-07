2a1522d8f5f5c8406da30a651983f29de5738605
Jawad Chowdhury <jawadc13@gmail.com>
2021-09-07 03:23:19 -0500

    Adding Styling, New Static Resources that are high definition, and Animations for front Page.

--- ExperiencePackage/lwc/homepage/homepage.js
+++ ExperiencePackage/lwc/homepage/homepage.js
@@ -1,58 +1,284 @@
-import { LightningElement, api, wire } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { LightningElement, wire, track} from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
 import { getRecord } from 'lightning/uiRecordApi';
-//import USER_ID from '@salesforce/user/Id';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getAttempts from '@salesforce/apex/UserAttempt.getAttempts';
+import getVouchers from '@salesforce/apex/UserAttempt.getVouchers';
 
+import style from '@salesforce/resourceUrl/style';
 import REVATURE_LOGO from '@salesforce/resourceUrl/RevatureLogo';
 import ADM_LOGO from '@salesforce/resourceUrl/AdmLogo';
+import ADVANCED_ADM_LOGO from "@salesforce/resourceUrl/AdvancedAdmLogo";
+import JAVASCRIPT_LOGO from "@salesforce/resourceUrl/JavscriptDevLogo";
+import PD1 from "@salesforce/resourceUrl/Pd1Logo";
+import PD2 from "@salesforce/resourceUrl/Pd2Logo";
+import PAB from "@salesforce/resourceUrl/platformappbuilder";
 
+import Id from '@salesforce/user/Id';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 
-/*
-import ASSOCIATE_OBJECT from '@salesforce/schema/Associate';
-    import ASSOCIATE_FIRSTNAME from '@salesforce/schema/Associate.First_Name';
-    import ASSOCIATE_LASTNAME from '@salesforce/schema/Associate.Last_Name';
-    import ASSOCIATE_NICKNAME from '@salesforce/schema/Associate.Nickname';    
 
-import VOUCHER_OBJECT from '@salesforce/schema/Voucher';
-    import VOUCHER_CERTIFICATIONTYPE from '@salesforce/schema/Voucher.Certification_Type';
-    import VOUCHER_DUEDATE from '@salesforce/schema/Voucher.Due_Date';
-    import VOUCHER_EXPIRATIONDATE from '@salesforce/schema/Voucher.Expiration_Date';
-    import VOUCHER_STATUS from '@salesforce/schema/Voucher.Status';
-    import VOUCHER_VOUCHERCODE from '@salesforce/schema/Voucher.Voucher_Code';
-    import VOUCHER_VOUCHERTYPE from '@salesforce/schema/Voucher.Voucher_Type';
+import ASSOCIATE_OBJECT from '@salesforce/schema/Associate__c';
+    import ASSOCIATE_FIRSTNAME from '@salesforce/schema/Associate__c.First_Name__c';
+    import ASSOCIATE_LASTNAME from '@salesforce/schema/Associate__c.Last_Name__c';
+    import ASSOCIATE_NICKNAME from '@salesforce/schema/Associate__c.Nickname__c';
+    import ASSOCIATE_EMAIL from '@salesforce/schema/Associate__c.Email__c';    
+   // import VOUCHER_CERTIFICATIONTYPE from '@salesforce/schema/Associate__c.Voucher__c.Certification_Type__c';
+import VOUCHER_OBJECT from '@salesforce/schema/Voucher__c';
+    import VOUCHER_CERTIFICATIONTYPE from '@salesforce/schema/Voucher__c.Certification_Type__c';
+    import VOUCHER_DUEDATE from '@salesforce/schema/Voucher__c.Due_Date__c';
+    import VOUCHER_EXPIRATIONDATE from '@salesforce/schema/Voucher__c.Expiration_Date__c';
+    import VOUCHER_STATUS from '@salesforce/schema/Voucher__c.Status__c';
+    import VOUCHER_VOUCHERCODE from '@salesforce/schema/Voucher__c.Voucher_Code__c';
+    import VOUCHER_VOUCHERTYPE from '@salesforce/schema/Voucher__c.Voucher_Type__c';
 
-import ATTEMPT_OBJECT from '@salesforce/schema/Attempt';
-*/
+
+const upcomingExamsColumn = [
+    { label: 'Certification', fieldName: 'CertificationType'},
+    { label: 'Due Date', fieldName: 'DueDate', type: 'date'  },
+    { label: 'Voucher Code', fieldName: 'VoucherCode'},
+
+];
+
+
+const examResultsColumn = [
+    { label: 'Certification', fieldName: 'Certification_Type'},
+   // { label: 'Attempt Type', fieldName: 'AttemptType' },
+    { label: 'Date', fieldName: 'Date', type: 'date' },
+    { label: 'Passed?', fieldName: 'Passed', type: 'boolean' },
+    { label: 'Result', fieldName: 'Result', type: 'formula(percent)' },
+];
+
+const certificationColumn = [
+    { label: 'Certification', fieldName: 'Certification_Type'},
+    { label: 'Status', fieldName: 'Status'},
+    { label: 'Date', fieldName: 'Date', type: 'date' },
+    
+    
+   // { label: 'Attempt Type', fieldName: 'AttemptType' },
+    //{ label: 'Passed?', fieldName: 'Passed', type: 'boolean' },
+    //{ label: 'Result', fieldName: 'Result', type: 'formula(percent)' },
+
+
+];
+
+
 export default class Homepage extends LightningElement{
-    objectApiName = ACCOUNT_OBJECT;
-    fields = [NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD];
-    handleSuccess(event) {
-        const toastEvent = new ShowToastEvent({
-            title: "Account created",
-            message: "Record ID: " + event.detail.id,
-            variant: "success"
-        });
-        this.dispatchEvent(toastEvent);
+
+    connectedCallback() {
+        loadStyle(this, style)
+      }
+
+    @track adm_logo = ADM_LOGO;
+    @track adv_adm_logo = ADVANCED_ADM_LOGO;
+    @track js_logo = JAVASCRIPT_LOGO;
+    @track rev_logo = REVATURE_LOGO;
+    @track pd1_logo = PD1;
+    @track pd2_logo = PD2;
+    @track pab_logo = PAB;
+
+    certificationColumn = certificationColumn;
+    upcomingExamsColumn = upcomingExamsColumn;
+    examResultsColumn = examResultsColumn;
+    userId = Id;
+    //@api Id_test = "a003F000005ETORQA4";
+    data;
+    error;
+    //@wire(getRecord, { recordId: Id_test, fields: [ASSOCIATE_FIRSTNAME, ASSOCIATE_LASTNAME, ASSOCIATE_EMAIL, VOUCHER_CERTIFICATIONTYPE] })
+    //associate_test;
+    attemptFilterType = "Certification";
+    get certifiedAttempts() {
+        return [
+                 { label: 'Certification', value: 'Certification' },
+               ];
+            }
+
+
+    @track currentVoucher;
+    @track vouchers;
+    @track currentAttempt;
+    @track attempts;
+    @wire(getAttempts)
+    ShowExamResults({ error, data }) {
+        if (data) {
+            
+            this.attempts = data;
+            this.userName = data[0].Voucher__r.Associate__r.First_Name__c;
+            
+            let examResults = [];
+
+            data.forEach(attempt => {
+                
+                let examResult = {};
+                
+                examResult.Certification_Type = attempt.Voucher__r.Certification_Type__c;
+                examResult.AttemptType = attempt.Attempt_Type__c;
+                examResult.Date= attempt.Date__c;
+                examResult.Passed = attempt.Passed__c;
+                examResult.Result = attempt.Weighted__c.toFixed(2) + "%";
+             
+                    
+                if(examResult.AttemptType == this.attemptFilterType){
+                    examResults.push(examResult);
+                }
+                
+            });
+
+            this.attempts = examResults;
+            
+
+        } else if (error) {
+            console.log(error);
+        }
+    }
+   
+
+
+    data;
+    error;
+
+    @track currentVoucher;
+    @track vouchers;
+    //Get Exam Resultsfron Apex Class
+    @wire(getVouchers)
+    ShowUpcomingExams({ error, data }) {
+        if (data) {
+           
+            this.vouchers = data;
+            
+            let voucherResults = [];
+
+            data.forEach(voucher => {
+                
+                let voucherResult = {};
+                
+                voucherResult.CertificationType= voucher.Certification_Type__c;
+                voucherResult.DueDate = voucher.Due_Date__c;
+                voucherResult.VoucherCode = voucher.Voucher_Code__c;
+            
+                voucherResults.push(voucherResult);
+               
+                
+            });
+
+            this.vouchers = voucherResults;
+
+        } else if (error) {
+            console.log(error);
+        }
     }
 
-    @api 
-    recordId;
-    @wire(getRecord, { recordId: '$recordId', fields: [NAME_FIELD] })
-    account;
-    
-    rev_logo = REVATURE_LOGO;
-    adm_logo = ADM_LOGO;
+
+    data;
+    error;
+    //@wire(getRecord, { recordId: Id_test, fields: [ASSOCIATE_FIRSTNAME, ASSOCIATE_LASTNAME, ASSOCIATE_EMAIL, VOUCHER_CERTIFICATIONTYPE] })
+    //associate_test;
+    //attemptFilterType = "Certification";
 
 
-    
-   /* @wire userId = USER_ID;
 
-    @wire name = ASSOCIATE_FIRSTNAME;
-    @wire voucherCode = VOUCHER_VOUCHERCODE;
-    */
+    //ADM Certification Status
+    @track currentCert;
+    @track certs;
+@wire (getAttempts) ShowCertStatus({ error, data }) {
+        if (data) {
+            
+            this.certs = data;
+           
+            let examCerts = [];
+
+            data.forEach(cert => {
+                
+                let examCert = {};
+                
+                examCert.Certification_Type = cert.Voucher__r.Certification_Type__c;
+                //examCert.Certified = cert.Voucher__r.Certified__c;
+                examCert.DueDate = cert.Voucher__r.Due_Date__c;
+
+                examCert.AttemptType = cert.Attempt_Type__c;
+                
+                examCert.Passed = cert.Passed__c;
+                examCert.Result = cert.Weighted__c.toFixed(2) + "%";
+
+                  let i = 0;  
+                breakme: if(examCert.AttemptType == this.attemptFilterType){
+                    if(examCert.Passed == true){
+                        examCert.Status = "Achieved";
+                        examCert.Date= cert.Date__c;
+                        examCerts.push(examCert);
+                        i = i + 1;
+                        break breakme;
+                    }
+                    else if(examCert.DueDate != null)
+                    {
+                        examCert.Status = "Pending";
+                        examCert.Date= cert.Voucher__r.Due_Date__c;
+                        examCerts.push(examCert);
+                        i = i + 1;
+                        break breakme;
+
+                    }
+                    
+                }
+
+            });
+
+            this.certs = examCerts;
+         
+
+        } else if (error) {
+            console.log(error);
+        }
+    }
+
+    //Advanced ADM
+ 
+
     
 }
+
+/*
+    //error check
+    wiredAccount({data, error}) {
+        console.log('Execute logic each time a new value is provisioned');
+        if (data) {
+            this.data = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.data = undefined;
+        }
+    }
+  */
+    //  objectApiNamey = ACCOUNT_OBJECT;
+  //  fieldsy = [NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD];
+   // @api objectApiName;
+
+
+ 
+
+  
+
+
+
+    
+    
+
+
+    
+//@api userId = USER_ID;
+
+ // @api associate = ASSOCIATE_OBJECT;
+ // @api associateFields = [ASSOCIATE_FIRSTNAME, ASSOCIATE_LASTNAME, ASSOCIATE_NICKNAME, ASSOCIATE_EMAIL];
+ //@api testName = ASSOCIATE_FIRSTNAME;
+ // @track voucher = VOUCHER_OBJECT;
+ // @track voucherFields = [VOUCHER_CERTIFICATIONTYPE, VOUCHER_DUEDATE, VOUCHER_EXPIRATIONDATE, VOUCHER_STATUS, VOUCHER_VOUCHERCODE, VOUCHER_VOUCHERTYPE];
+   
+ //  @track attempt=ATTEMPT_OBJECT;
+ //  @wire voucherCode = VOUCHER_VOUCHERCODE;
+
+    
+
