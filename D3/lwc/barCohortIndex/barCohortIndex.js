02f41529e86af387929044c092d607206017e833
zendpen <47953858+zendpen@users.noreply.github.com>
2021-09-09 11:48:25 -0700

    Update barCohortIndex.js

--- D3/lwc/barCohortIndex/barCohortIndex.js
+++ D3/lwc/barCohortIndex/barCohortIndex.js
@@ -1,3 +1,15 @@
+/*
+ * Name: Jacob Schwartz
+ * Date: September 9, 2021
+ * Project3: Achievement Unlocked
+ * Description: This component was made in preparation for adding an overview of associate data with the bar graphs.
+ * This CMP WAS NOT USED.
+ * 
+ * The Plan: Similar to the cohortBarGraphIndex component. Write a callout to an apex controller that passes back overview data and then format it
+ * into an array to pass to the barGraphIndex component.
+*/
+
+
 import { LightningElement } from 'lwc';
 
 export default class BarCohortIndex extends LightningElement {
