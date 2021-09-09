84d1e78a16fc686ced55b8c2e1dd0f9c8ac1610a
gregory-mannerberg <86627319+gregory-mannerberg@users.noreply.github.com>
2021-09-09 15:06:24 -0400

    Update cohortView.js

--- lwcInternal/lwc/cohortView/cohortView.js
+++ lwcInternal/lwc/cohortView/cohortView.js
@@ -1,4 +1,17 @@
+/*
+Name: cohortView
+Author(s): Greg Mannerberg
+Description: Client side controller for the cohort view.
+*/
 import {LightningElement, wire} from 'lwc';
+/* These import statements can be used to enable Lightning Message Service to
+ * communicate with D3 charts. This was not completed in the initial iteration.
+ * The messageChannel CohortSelected__c may have been lost in a branch
+ * that was not merged before a code freeze and then deleted. This messageChannel was
+ * in the D3 package to maintain the dependence direction of lwcInternal being
+ * dependent on D3. A copy can be found at:
+ * https://github.com/revaturelabs/AchievementUnlocked/pull/167/files
+ */
 //import {MessageContext, publish} from 'lightning/messageService';
 //import COHORT_SELECTED_CHANNEL from '@salesforce/messageChannel/CohortSelected__c';
 
@@ -13,6 +26,7 @@ export default class CohortView extends LightningElement {
         this.filter = event.detail.value;
     }
 
+    // Gets the combobox options for the cohort filter
     get options() {
         return [
             {label: 'All', value: 'all'},
@@ -21,6 +35,8 @@ export default class CohortView extends LightningElement {
         ]
     }
 
+    // An event handler for when a cohort is selected that sets cohortId to the selected
+    // cohort's id.
     displayCohort(event) {
         this.cohortId = event.detail;
         /*const payload = {
@@ -30,6 +46,8 @@ export default class CohortView extends LightningElement {
         */
     }
 
+    // An event handler for when an accordion section is selected. Clears cohortId if 
+    // the cohorts section is selected.
     handleToggleSection(event) {
         const openSections = event.detail.openSections;
         if (openSections == 'cohorts') {
@@ -42,6 +60,8 @@ export default class CohortView extends LightningElement {
         }
     }
 
+    // Lifecycle function. Sets the associates accordion section to display 
+    // if cohortId is set to an id.
     renderedCallback() {
         if (this.cohortId != null) {
             const accordion = this.template.querySelector('.accordion');
